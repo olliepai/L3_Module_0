93a82dd8be2205cf7732e5ad188b3db69c7aae18
Ollie Pai <ollie.pai2003@gmail.com>
2017-08-15 16:54:10 -0700

    Day 1

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -1,6 +1,23 @@
 package IntroToArrayLists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener{
+	JButton addNameButton;
+	JButton viewNameButton;
+	ArrayList<String> guestNames = new ArrayList<String>();
+	
+	public static void main(String[] args) {
+		GuestBook guestBook = new GuestBook();
+		guestBook.GUI();
+	}
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +26,41 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	
+	public void GUI() {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JPanel panel = new JPanel();
+		panel.setVisible(true);
+		frame.add(panel);
+		addNameButton = new JButton();
+		viewNameButton = new JButton();
+		addNameButton.setText("Add Name");
+		viewNameButton.setText("View Names");
+		addNameButton.addActionListener(this);
+		viewNameButton.addActionListener(this);
+		panel.add(addNameButton);
+		panel.add(viewNameButton);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String list = "";
+		
+		// TODO Auto-generated method stub
+		if (e.getSource().equals(addNameButton)) {
+			String enterName = JOptionPane.showInputDialog("Enter a name.");
+			guestNames.add(enterName);
+			
+		}
+		
+		if (e.getSource().equals(viewNameButton)) {
+			for (int i = 0; i < guestNames.size(); i++) {
+				list = list + "\n Guest #" + (i + 1) + ": " + guestNames.get(i);
+			}
+			JOptionPane.showMessageDialog(null, list);
+		}
+	}
 }
--- src/IntroToArrayLists/IntroToArrayLists.java
+++ src/IntroToArrayLists/IntroToArrayLists.java
@@ -1,20 +1,41 @@
 package IntroToArrayLists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
-		
-		//2. Add five Strings to your list
-		
-		//3. Print all the Strings using a standard for-loop
-		
-		//4. Print all the Strings using a for-each loop
-		
-		//5. Print only the even numbered elements in the list.
-		
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
+		ArrayList<String> stringArray = new ArrayList<String>();
+		// 2. Add five Strings to your list
+		stringArray.add("String #1");
+		stringArray.add("String #2");
+		stringArray.add("String #3");
+		stringArray.add("String #4");
+		stringArray.add("String #5 e");
+		// 3. Print all the Strings using a standard for-loop
+		for (int i = 0; i < stringArray.size(); i++) {
+			System.out.println(stringArray.get(i));
+		}
+		// 4. Print all the Strings using a for-each loop
+		for (String s : stringArray) {
+			System.out.println(s);
+		}
+		// 5. Print only the even numbered elements in the list.
+		for (int i = 0; i < stringArray.size(); i++) {
+			if (i % 2 == 0) {
+				System.out.println(stringArray.get(i));
+			}
+		}
+		// 6. Print all the Strings in reverse order.
+		for (int i = stringArray.size() - 1; i > - 1; i--) {
+			System.out.println(stringArray.get(i));
+		}
+		// 7. Print only the Strings that have the letter 'e' in them.
+		for (String s : stringArray) {
+			if (s.contains("e")) {
+				System.out.println(s);
+			}
+		}
 	}
 }
